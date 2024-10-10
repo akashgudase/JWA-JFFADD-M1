785a9ff14d2fc5e405d82cce2c65d3ba3aba50d1
Akash Kiran Gudase <130822349+akashgudase@users.noreply.github.com>
2024-10-10 09:49:08 +0530

    Add files via upload

---
+++ contactmanager/src/com/jspiders/contactmanager/business/Business.java
@@ -0,0 +1,15 @@
+package com.jspiders.contactmanager.business;
+
+public interface Business {
+	
+	void addContact();
+	
+	void deleteContact();
+	
+	void updateContact();
+	
+	void findContactByFirstName();
+	
+	void findContactByLastName();
+
+}
