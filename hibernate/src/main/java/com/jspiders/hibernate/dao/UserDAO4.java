89acd4890c8c61e5ca33a483a6824c775e91330f
Akash Kiran Gudase <130822349+akashgudase@users.noreply.github.com>
2024-11-30 09:22:02 +0530

    Add files via upload

---
+++ hibernate/src/main/java/com/jspiders/hibernate/dao/UserDAO4.java
@@ -0,0 +1,46 @@
+package com.jspiders.hibernate.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.jspiders.hibernate.dto.UserDTO;
+
+public class UserDAO4 {
+
+	private static EntityManagerFactory entityManagerFactory;
+	private static EntityManager entityManager;
+	private static EntityTransaction entityTransaction;
+
+	public static void main(String[] args) {
+		openConnection();
+		UserDTO userDTO = entityManager.find(UserDTO.class, 1);
+		if (userDTO != null) {
+			userDTO.setMobile(9876543210l);
+			entityTransaction.begin();
+			entityManager.persist(userDTO);
+			entityTransaction.commit();
+		} else
+			System.out.println("User not found");
+		closeConnection();
+	}
+
+	private static void openConnection() {
+		entityManagerFactory = Persistence.createEntityManagerFactory("hibernate");
+		entityManager = entityManagerFactory.createEntityManager();
+		entityTransaction = entityManager.getTransaction();
+	}
+
+	private static void closeConnection() {
+		if (entityManagerFactory != null)
+			entityManagerFactory.close();
+		if (entityManager != null)
+			entityManager.close();
+		if (entityTransaction != null) {
+			if (entityTransaction.isActive())
+				entityTransaction.rollback();
+		}
+	}
+
+}
