76254b6ce3580ccf3447adff7a4009d616281d85
Akash Kiran Gudase <130822349+akashgudase@users.noreply.github.com>
2024-12-02 09:04:03 +0530

    Add files via upload

---
+++ hibernate/src/main/java/com/jspiders/hibernate/dto/Company.java
@@ -0,0 +1,27 @@
+package com.jspiders.hibernate.dto;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Company {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String location;
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private List<Employee> employees;
+
+}
