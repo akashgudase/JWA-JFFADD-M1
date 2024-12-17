44ab17c1caddd50f3f7bd966be4cd61874db93f3
Akash Kiran Gudase <130822349+akashgudase@users.noreply.github.com>
2024-12-17 09:29:57 +0530

    Add files via upload

---
+++ servlet/src/main/java/com/jspiders/servlet/MyServlet2.java
@@ -0,0 +1,31 @@
+package com.jspiders.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet2 extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		writer.println("Response from MyServlet2");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		writer.println("User is trying to login using username " + username + " and password " + password);
+	}
+
+}
