f31018d14f4b673d284b7af214e10e2721aeb81a
Akash Kiran Gudase <130822349+akashgudase@users.noreply.github.com>
2024-12-25 10:07:27 +0530

    Add files via upload

---
+++ springcore/src/main/java/com/jspiders/springcore/bean/BeanOperation.java
@@ -0,0 +1,16 @@
+package com.jspiders.springcore.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component(value = "operation")
+public class BeanOperation {
+
+//	@Autowired
+//	private BeanManager beanManager;
+
+//	@Override
+//	public String toString() {
+//		return "BeanOperation [beanManager=" + beanManager + "]";
+//	}
+
+}
---
+++ springcore/src/main/java/com/jspiders/springcore/bean/Gmail.java
@@ -0,0 +1,8 @@
+package com.jspiders.springcore.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component(value = "gmail")
+public class Gmail implements Messanger {
+
+}
